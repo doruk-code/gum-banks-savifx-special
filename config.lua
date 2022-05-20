064b4b982fcda7133cdf47d6480e93225eb520bc
Doruk Kılıç <98923085+savilah@users.noreply.github.com>
2022-05-20 05:12:53 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,61 @@
+Config = {}
+
+Config.webhook = "your webhook"
+
+Banks = {
+    [1] =  {
+        ["Enable"] = true,
+        ["City_Name"] = "Valentine",
+        ["Blip_Name"] = "Valentine Bank",
+        ["Coords"] = {x=-308.50,y=776.24,z=118.75, r=2.0},
+    },
+    [2] =  {
+        ["Enable"] = true,
+        ["City_Name"] = "Blackwater",
+        ["Blip_Name"] = "Blackwater Bank",
+        ["Coords"] = {x=-813.18,y=-1277.60,z=43.68, r=2.0},
+    },
+    [3] =  {
+        ["Enable"] = true,
+        ["City_Name"] = "Saint Denis",
+        ["Blip_Name"] = "Saint Denis Bank",
+        ["Coords"] = {x=2644.08,y=-1292.21,z=52.29, r=2.0},
+    },
+    [4] =  {
+        ["Enable"] = true,
+        ["City_Name"] = "Saint Denis",
+        ["Blip_Name"] = "Saint Denis Bank",
+        ["Coords"] = {x=1292.51,y=-1301.74,z=76.09, r=2.0},
+    },
+}
+
+Config.Text_Enter = "Bankaya girmek icin ~e~[ G ]~q~ tusuna basin "
+Config.Waiting_Text = "Someone is close."
+Config.Loading_Text = "Banka Yuklenemedi tekrar deneyiniz."
+Config.Bank_Now = "Var :"
+Config.Bank_Add_Gold = "Bankaya ekledin :"
+Config.Bank_Remove_Money = "Banka formu aldin :"
+Config.Bank_Create_Account = "Burada hesap olusturdunuz :"
+Config.Banker_Have_Job = "Bankaci bir seyle ilgileniyor biraz bekleyin"
+Config.Dont_Have = "Senin hesabin yok : "
+Config.Bank_Book = "Western Bank"
+Config.Text_Sub = "Parani guvende tut"
+Config.City_Text = "Sehir : "
+Config.Name_Text = "isim' : "
+Config.LName_Text = "soyad : "
+Config.Money_Text = "para : "
+Config.Gold_Text = "altin : "
+Config.Add_Money_To_Bank = "para yatir "
+Config.Take_Money_To_Bank = "Para cek"
+Config.Send_Money_Text = "para yatir"
+Config.Send_Money_Text1 = "Ne kadar para yatiracaksiniz"
+Config.Take_Money_From_Bank = "para cek"
+Config.Take_Money_From_Bank2 = "Ne kadar para cekeceksiniz"
+Config.NotCoolValue = "Gecersiz deger"
+Config.Add_Gold_To_Bank = "Deposit gold"
+Config.Add_Gold_To_Bank2 = "Deposit gold"
+Config.Add_Gold_To_Bank3 = "How much gold do you need Deposit?"
+Config.Remove_Gold_To_Bank = "Withdraw gold"
+Config.Remove_Gold_To_Bank2 = "Withdraw gold"
+Config.Remove_Gold_To_Bank3 = "How much gold do you need take?"
+Config.Leave_Bank = "bankadan cik"
